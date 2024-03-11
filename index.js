96329da0c2d4e1014b09bf9d6125c81a27a024d2
Clint Baker <clintjbaker@gmail.com>
2024-03-11 17:05:48 -0600

    clean up code with better logging

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ import axios from 'axios'
 
 console.log('GO FOR REQBRO')
 
-function reqbro(url, interval) {
-  const interval = setInterval(callback, interval, url)
+function reqbro(url, time) {
+  callback(url)
+  const interval = setInterval(callback, time, url)
 }
 
 async function callback(url) {
-  console.log('getting url')
   const res = await axios.get(url)
   console.log(res)
+  if (res.status === 200) {
+    const now = new Date()
+    console.log('url fetched! at ' + now)
+  } else {
+    throw new Error('Unable to fetch URL')
+  }
 }
 
-reqbro('https://gcr-7rlp.onrender.com/', 3600000)
+reqbro('https://gcr-7rlp.onrender.com/', 840000)
