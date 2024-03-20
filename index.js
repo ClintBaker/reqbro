dee93c34f0cefcf8de5f7b9c1b5850084c09bfe0
Clint Baker <clintjbaker@gmail.com>
2024-03-20 15:47:19 -0600

    improve logging and layout

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ function reqbro(url, time) {
 async function callback(url) {
   const res = await axios.get(url)
   if (res.status === 200) {
-    const now = new Date()
-    console.log('url fetched! at ' + now)
+    const now = new Date().toLocaleDateString()
+    console.log(`${url} fetched! at ` + now)
   } else {
     throw new Error('Unable to fetch URL')
   }
 }
 
+// GOLF COURSE RANKER EVERY 14 MINUTES
 reqbro('https://gcr-7rlp.onrender.com/', 840000)
